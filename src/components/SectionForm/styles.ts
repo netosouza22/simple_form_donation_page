b11640702c5a37fc407da628d46e665dd2541e73
Antônio Assis de Sousa Neto <netosouzaifce@gmail.com>
2022-05-10 10:05:00 -0300

    style: adição de estilização do arquivo sectionform

--- src/components/SectionForm/styles.ts
+++ src/components/SectionForm/styles.ts
@@ -1,19 +1,31 @@
 import styled from 'styled-components';
 
-export const ContainerForm = styled.div``;
+export const ContainerForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
 export const Container = styled.div`
   width: 50%;
   height: 100%;
+`;
+
+export const ContainerRadioForm = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const RadioMoneySection = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  margin: 0px 0px 10px 0px;
 `;
 export const RadioMoneyGroupLabel = styled.h3`
   font-size: 1.6rem;
   align-self: flex-start;
+  margin-bottom: 5px;
 `;
 
 export const RadioMoneyGroup = styled.div`
