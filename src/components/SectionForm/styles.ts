1e56d93127587db5607eed15f2ed39c49320c546
Antônio Assis de Sousa Neto <netosouzaifce@gmail.com>
2022-10-05 11:44:32 -0300

    styles: changed the reference in styled components of the component 'containerForm' now it is used like a 'form' and not as 'div' like before

--- src/components/SectionForm/styles.ts
+++ src/components/SectionForm/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const ContainerForm = styled.div`
+export const ContainerForm = styled.form`
   display: flex;
   flex-direction: column;
   gap: 5px;
